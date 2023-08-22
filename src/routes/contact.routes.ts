a18cb44da0e771bb29c29becf25140f22d4b24ff
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-08-22 10:46:58 +0500

    imported to index from contact router

--- src/routes/contact.routes.ts
+++ src/routes/contact.routes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import ContactController from '../controllers/contact.contr';
+import ContactController from '../controllers/contact.contr.js';
 import authMiddleware from '../middleware/auth';
 let router = express.Router();
 router.get('/', ContactController.getContacts)
--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import contactRouter from "./contact.routes.js"
 const router = express.Router();
 router.use('/test', () => { });
 export default router
